0be3c7ae0ea5cdb9362ec753df5cf66ec88666b3
EC2 Default User <ec2-user@ip-172-31-35-128.ap-northeast-1.compute.internal>
2023-10-15 12:21:06 +0000

    [update]ページネイション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-
 
   private
 
